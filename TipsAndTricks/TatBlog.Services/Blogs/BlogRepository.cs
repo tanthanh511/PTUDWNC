b8e399ca79c00602181c3b4761ccd49535ed4a53
tanthanh511 <117253637+tanthanh511@users.noreply.github.com>
2023-02-27 09:13:25 +0700

    TipsAndTricks
    còn sai sót ở menu

---
+++ TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
+
+namespace TatBlog.Services.Blogs
+{
+    public class BlogRepository : IBlogRepository
+    {
+        // cài đặt phương thức khỏi tạo cho blogRepossitiory
+        private readonly BlogDbContext _context;
+        public BlogRepository( BlogDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // **** điểm cần chú ý là throw new notImplementedException: đưa ra ngoài lệ cho một phương thức 
+
+        // tìm bài viết có định danh là (slug) và được đăng vào tháng năm nào đó ....
+        public async Task<Post> GetPostAsync(int year, int month, string slug, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Post> postsQuery = _context.Set<Post>()
+                .Include(x => x.Category)
+                .Include(x => x.Author);
+
+            if (year > 0)
+            {
+                postsQuery = postsQuery.Where(x => x.PostedDate.Year == year);
+            }
+
+            if (month > 0)
+            {
+                postsQuery = postsQuery.Where(x => x.PostedDate.Month == month);
+            }
+            if (!string.IsNullOrWhiteSpace(slug))
+            {
+                postsQuery = postsQuery.Where(x => x.UrlSlug == slug);
+            }
+            // bất đồng bộ sử dụng await để tách hai hàm ra với nhau dể sử lí hơn 
+            return await postsQuery.FirstOrDefaultAsync(cancellationToken);
+           // throw new NotImplementedException();
+        }
+
+        // thực thi câu truy vấn 
+   
+
+        // tìm một bài post top N bài viết được moiij người xem nhất 
+        public async Task<IList<Post>> GetPopularArticlesAsync(int numPosts, CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<Post>()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderByDescending(p => p.Viewcount)
+                .Take(numPosts)
+                .ToListAsync(cancellationToken);
+            //throw new NotImplementedException();
+        }       
+
+        // tăng(view)  cho một bài viết 
+        public async Task IncreaseViewCountAsync(int postID, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<Post>()
+           .Where(x => x.Id == postID)
+           .ExecuteUpdateAsync(p =>
+           p.SetProperty(x => x.Viewcount, x => x.Viewcount + 1), cancellationToken);
+           // throw new NotImplementedException();
+        }
+
+    
+
+
+        // kiểm tra xem tên định danh của bài viết đã có hay chưa có 
+        public async Task<bool> IsPostSlugExistedAsync(int postID, string slug, CancellationToken cancellationToken = default)
+        {
+
+            return await _context.Set<Post>()
+               .AnyAsync(x => x.Id != postID && x.UrlSlug == slug, cancellationToken);
+            //throw new NotImplementedException();
+        }
+
+      
+    }
+}
---
+++ TipsAndTricks/TatBlog.Services/Blogs/IBlogRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs;
+
+public interface IBlogRepository
+    //< cái này chỉ mới tạo hàm để khởi tạo và chạo đa luồng >
+{   // CancellationToken cancellationToken = default ( mã thông báo hủy) >_<
+
+    // tìm bài viết có định danh là slug 
+    // được đăn vào tháng và năm 
+    Task<Post> GetPostAsync(
+        int year,
+        int month,
+        string slug,
+        CancellationToken cancellationToken = default);
+
+    // tim top N bai viet pho bien duoc nhiu nguoi xem nha
+    // task chức năng giống thread cũng là xử lí đa luồng nhưng task hỗ trợ thư viện sẵn nên dùng task lun ^_^
+    Task<IList<Post>> GetPopularArticlesAsync(
+        int numPosts,
+        CancellationToken cancellationToken = default);
+
+    // kiểm tra xem tên của bài viết đã có hay chưa 
+    Task<bool> IsPostSlugExistedAsync(
+        int postID, string slug,
+        CancellationToken cancellationToken = default);
+
+    // tăng số lượng xem của một bài viết 
+    Task IncreaseViewCountAsync(
+        int postID,
+        CancellationToken cancellationToken = default);
+       
+    //
+
+        
+}
